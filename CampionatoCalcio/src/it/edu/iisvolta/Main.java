a098094b1a1f0044f1420e1c0bac11a49037e7ef
Serale <Serale@sistemiPTpc11>
2023-03-31 20:59:36 +0200

    Aggiunta classe Main

--- CampionatoCalcio/src/it/edu/iisvolta/Main.java
+++ CampionatoCalcio/src/it/edu/iisvolta/Main.java
@@ -9,13 +9,24 @@ public class Main {
 		c.getElencoSquadre().add(new Squadra("Napoli"));
 		c.getElencoSquadre().add(new Squadra("Roma"));
 		c.getElencoSquadre().add(new Squadra("Lazio"));
+		c.getElencoSquadre().add(new Squadra("Fanfulla Lodi"));
 		
 		c.generaCalendario();
 		
-		for(Partita p:c.getElencoPartite())
+		for(Partita partita:c.getElencoPartite()) {
+			partita.simulaPartita();
+			
 			System.out.println(
-				p.getSquadraCasa().getNome()+ " - "+ 
-				p.getSquadraFuori().getNome());
+					partita.getSquadraCasa().getNome()+ " - "+ 
+					partita.getSquadraFuori().getNome()+ " : "+
+					partita.getGolCasa()+" - "+ 
+					partita.getGolFuori());
+		}
+			
+		
+		System.out.println("\n\nCLASSIFICA: ");
+		for(Squadra s:c.getElencoSquadre()) 
+			System.out.println(s.getNome()+": "+s.getPunti());
 	}
 
 }
